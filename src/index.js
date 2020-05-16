8a3915ba6883595270ad11b8178bcf48c98ee07e
Parakhina Maria <mickeymouse0243@gmail.com>
2020-05-17 00:42:32 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+import '@babel/polyfill';
+import 'nodelist-foreach-polyfill';
+import elementClosest from 'element-closest';
+import 'fetch-polyfill';
+elementClosest(window);
+require('formdata-polyfill');
+require('es6-promise/auto');
+
 import callMe from './modules/callMe.js';
 import accordion from './modules/accordion.js';
 import calculatorAccordion from './modules/calculatorAccordion.js';
